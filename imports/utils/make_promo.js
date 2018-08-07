550cae91d9a4179de7cb4e6e32e2fcc2855fc3db
tollevin <tollevin@getfednyc.com>
2018-08-07 13:09:35 -0400

    Added email, promo creation

---
+++ imports/utils/make_promo.js
@@ -0,0 +1,9 @@
+export const makeAmbassadorPromo = () => {  let text = '';
+  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+  for (let i = 0; i < 6; i += 1) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+
+  return text;
+};
