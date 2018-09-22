7f007217044dcb0627fcacdaf6acd79aef6b4390
Greg <gregt87@gmail.com>
2018-09-22 17:58:07 -0400

    added more information bugsnag rules

--- imports/startup/client/bugsnag.js
+++ imports/startup/client/bugsnag.js
@@ -2,6 +2,7 @@ import bugsnag from 'bugsnag-js';
 
 const bugsnagClient = bugsnag('ed63bdfaf7876bf7db58e9076bafb27b');
 
+/* eslint-disable */
 window.onerror = (message, url, lineNumber, columnNumber, error) => {
   const errorFull = `
     url: ${url}
@@ -12,3 +13,32 @@ window.onerror = (message, url, lineNumber, columnNumber, error) => {
     `;
   bugsnagClient.notify(new Error(errorFull));
 };
+
+const originalMeteorError = Meteor.Error;
+
+Meteor.Error = function (error, reason, details) {
+  const errorFull = `
+    Meteor Error: 
+    reason: ${reason}
+    details: ${details}
+    error: ${error}
+    `;
+  bugsnagClient.notify(new Error(errorFull));
+
+  return originalMeteorError.apply(this, arguments);
+};
+
+const originalBlazeCatchingExceptions = Blaze._reportException;
+
+Blaze._reportException = function (error, where) {
+  const errorFull = `
+    Blaze Error: 
+    where: ${where}
+    message: ${error.message}
+    stack: ${error.stack}
+    `;
+  bugsnagClient.notify(new Error(errorFull));
+
+  return originalBlazeCatchingExceptions.apply(this, arguments);
+};
+/* eslint-enable */
