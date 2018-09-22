6245749e33d7e22a16068a45b1dd322865eafcf6
Greg <Gregt87@gmail.com>
2018-09-22 18:34:36 -0400

    added more information bugsnag rules (#41)
    * added more information bugsnag rules
    
    * removed .DS_Store I FUCKING HATE THAT FILE
    
    * does not run bugsnag in dev mode
    
    * wrapped the function to early exit if in dev mode

--- imports/startup/client/bugsnag.js
+++ imports/startup/client/bugsnag.js
@@ -1,14 +1,47 @@
 import bugsnag from 'bugsnag-js';
 
-const bugsnagClient = bugsnag('ed63bdfaf7876bf7db58e9076bafb27b');
+(() => {
+  const bugsnagClient = bugsnag('ed63bdfaf7876bf7db58e9076bafb27b');
 
-window.onerror = (message, url, lineNumber, columnNumber, error) => {
-  const errorFull = `
+  if (window.location.hostname === 'localhost') { return; }
+  /* eslint-disable */
+  window.onerror = (message, url, lineNumber, columnNumber, error) => {
+    const errorFull = `
     url: ${url}
     message: ${message}
     lineNumber ${lineNumber}
     columnNumber ${columnNumber}
     error ${error}
     `;
-  bugsnagClient.notify(new Error(errorFull));
-};
+    bugsnagClient.notify(new Error(errorFull));
+  };
+
+  const originalMeteorError = Meteor.Error;
+
+  Meteor.Error = function (error, reason, details) {
+    const errorFull = `
+    Meteor Error: 
+    reason: ${reason}
+    details: ${details}
+    error: ${error}
+    `;
+    bugsnagClient.notify(new Error(errorFull));
+
+    return originalMeteorError.apply(this, arguments);
+  };
+
+  const originalBlazeCatchingExceptions = Blaze._reportException;
+
+  Blaze._reportException = function (error, where) {
+    const errorFull = `
+    Blaze Error: 
+    where: ${where}
+    message: ${error.message}
+    stack: ${error.stack}
+    `;
+    bugsnagClient.notify(new Error(errorFull));
+
+    return originalBlazeCatchingExceptions.apply(this, arguments);
+  };
+  /* eslint-enable */
+})();
--- imports/startup/server/monti.js
+++ imports/startup/server/monti.js
@@ -1,1 +1,3 @@
+/* eslint-disable */
 Kadira.connect('xc5myAnQ8HNhtqGxu', 'ad8012d1-e6b8-45e0-83e3-a83200fd2b9d');
+/* eslint-enable */
