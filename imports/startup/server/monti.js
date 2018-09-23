da36f34ac3a9033c1bc546ae7fca87987b01ef3b
Greg <Gregt87@gmail.com>
2018-09-22 23:02:20 -0400

    turns of monti in dev mode (#44)

--- imports/startup/server/monti.js
+++ imports/startup/server/monti.js
@@ -1,3 +1,8 @@
 /* eslint-disable */
-Kadira.connect('xc5myAnQ8HNhtqGxu', 'ad8012d1-e6b8-45e0-83e3-a83200fd2b9d');
+(() => {
+  const env = process.env.ROOT_URL;
+  const devUrl = "http://localhost";
+  if (env && env.indexOf(devUrl) !== -1) { return; }
+  Kadira.connect('xc5myAnQ8HNhtqGxu', 'ad8012d1-e6b8-45e0-83e3-a83200fd2b9d');
+})();
 /* eslint-enable */
