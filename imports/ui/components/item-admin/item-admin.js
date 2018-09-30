e1d772346536aac1ef7d4201242bbfbdc5b5e176
Greg <gregt87@gmail.com>
2018-09-30 16:16:13 -0400

    fixes menu errors

--- imports/ui/components/item-admin/item-admin.js
+++ imports/ui/components/item-admin/item-admin.js
@@ -4,29 +4,20 @@ import { remove } from '/imports/api/items/methods.js';
 
 import './item-admin.html';
 
+const trueValues = (obj) =>
+  Object.entries(obj)
+    .filter(([_, hasVal]) => hasVal)
+    .map(([keyName, _]) => keyName);
+
 Template.Item_admin.helpers({
   trueWarnings: () => {
-    const { warnings } = Template.currentData();
-    const warningsKeys = Object.keys(warnings);
-    const trueWarnings = [];
-
-    for (let i = warningsKeys.length - 1; i >= 0; i -= 1) {
-      if (warnings[warningsKeys[i]]) trueWarnings.push(warningsKeys[i]);
-    }
-
-    return trueWarnings;
+    const warnings = Template.currentData().warnings || {};
+    return trueValues(warnings);
   },
 
   trueAttributes: () => {
-    const { attributes } = Template.currentData();
-    const attributesKeys = Object.keys(attributes);
-    const trueAttributes = [];
-
-    for (let i = attributesKeys.length - 1; i >= 0; i -= 1) {
-      if (attributes[attributesKeys[i]]) trueAttributes.push(attributesKeys[i]);
-    }
-
-    return trueAttributes;
+    const attributes = Template.currentData().attributes || {};
+    return trueValues(attributes);
   },
 });
 
